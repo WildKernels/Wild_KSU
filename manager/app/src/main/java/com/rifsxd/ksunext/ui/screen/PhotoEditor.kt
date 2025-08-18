7fedf48ddee0cf7c18955b04ff5cfbf3395a08d8
TheWildJames <bins4us@hotmail.com>
2025-08-18 01:17:32 -0400

    Feat: Center icons in bottom bar and adjust padding

--- manager/app/src/main/java/com/rifsxd/ksunext/ui/screen/PhotoEditor.kt
+++ manager/app/src/main/java/com/rifsxd/ksunext/ui/screen/PhotoEditor.kt
@@ -428,8 +428,8 @@ fun PhotoEditor(
                  Row(
                      modifier = Modifier
                          .fillMaxWidth(0.5f)
-                         .padding(start = 16.dp),
-                     horizontalArrangement = Arrangement.spacedBy(8.dp),
+                         .padding(horizontal = 8.dp),
+                     horizontalArrangement = Arrangement.Center,
                      verticalAlignment = Alignment.CenterVertically
                  ) {
 
