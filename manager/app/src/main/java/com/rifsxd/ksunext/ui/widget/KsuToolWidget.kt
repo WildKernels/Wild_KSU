19ff08f42761830b7cda8fc9c8bac5316346da01
TheWildJames <bins4us@hotmail.com>
2026-01-12 00:38:14 -0500

    feat(widgets): add new home screen widgets for KsuTool, KPatch and Zygisk
    Add three new widget classes and their corresponding XML configurations
    to provide quick access to KsuTool, KPatch and Zygisk features from home screen

---
+++ manager/app/src/main/java/com/rifsxd/ksunext/ui/widget/KsuToolWidget.kt
@@ -0,0 +1,9 @@
+package com.rifsxd.ksunext.ui.widget
+
+import com.rifsxd.ksunext.R
+
+class KsuToolWidget : BaseIconWidget(
+    iconResId = R.drawable.ic_toolkit,
+    action = "ACTION_KSUTOOL",
+    labelResId = R.string.shortcut_ksutool
+)
